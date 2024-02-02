cb69afdd1f0f184327395c32f61b64bce24c627c
Kelly Mao <74526299+kellycmao12@users.noreply.github.com>
2024-02-02 11:58:14 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,64 @@
+document.addEventListener("DOMContentLoaded", function() {
+    window.addEventListener("scroll", function() {
+        var scrollPosition = window.scrollY;
+
+        // Adjust the threshold value as needed
+        if (scrollPosition >= 200) {
+            document.getElementById("project-slide-1").style.transform = "translateX(0)";
+            document.getElementById("label-slide-1").style.transform = "translateX(0)";
+        }
+
+        // Adjust the threshold value as needed
+        if (scrollPosition >= 600) {
+            document.getElementById("project-slide-2").style.transform = "translateX(0)";
+            document.getElementById("label-slide-2").style.transform = "translateX(0)";
+        }
+
+        // Adjust the threshold value as needed
+        if (scrollPosition >= 1000) {
+            document.getElementById("project-slide-3").style.transform = "translateX(0)";
+            document.getElementById("label-slide-3").style.transform = "translateX(0)";
+        }
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    var nyc = document.getElementById('nyc');
+    var nycImage = document.querySelector(".nyc-image");
+
+    var sf = document.getElementById('sf');
+    var sfImage = document.querySelector(".sf-image");
+
+    var creativeTech = document.getElementById('creative-tech');
+    var creativeTechImage = document.querySelector(".creative-tech-image");
+
+    nyc.addEventListener('mouseover', function() {
+        nyc.style.color = '#FF249A';
+        nycImage.style.display = "block";
+    });
+
+    nyc.addEventListener('mouseout', function() {
+        nyc.style.color = '#333333';
+        nycImage.style.display = "none";
+    });
+
+    sf.addEventListener('mouseover', function() {
+        sf.style.color = '#FF922E';
+        sfImage.style.display = "block";
+    });
+
+    sf.addEventListener('mouseout', function() {
+        sf.style.color = '#333333';
+        sfImage.style.display = "none";
+    });
+
+    creativeTech.addEventListener('mouseover', function() {
+        creativeTech.style.color = '#679fff';
+        creativeTechImage.style.display = "block";
+    });
+
+    creativeTech.addEventListener('mouseout', function() {
+        creativeTech.style.color = '#333333';
+        creativeTechImage.style.display = "none";
+    });
+});
